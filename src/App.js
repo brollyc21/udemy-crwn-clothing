5dff00a503ce0309c82a65e5e97332e37637e04d
Rafael Carvalho <rafaelc21@hotmail.com>
2021-03-10 16:57:21 +0000

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/App.js
+++ src/App.js
@@ -44,7 +44,11 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/signIn' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />) } />
+          <Route exact path='/signIn' render={() => 
+            this.props.currentUser ? 
+              (<Redirect to='/' />) : 
+              (<SignInAndSignUpPage />) } 
+          />
         </Switch>
       </div>
     );
